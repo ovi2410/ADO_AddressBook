e277d1217de477e0bcf082a2f8a3ae33319c56b4
ovi2410 <guneshwarifegade@gmail.com>
2022-06-14 06:38:19 +0530

    [ovi]edit existing contact

--- ADO_AddressBook/uc3_editExistingcontact/ContactDataManager.cs
+++ ADO_AddressBook/uc3_editExistingcontact/ContactDataManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC2_insertintotableusingSP
+namespace uc3_editExistingcontact
 {
     public class ContactDataManager
     {
