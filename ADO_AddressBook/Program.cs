0b16f4618bc08bce5c3a34f5334dcc19155ce580
ovi2410 <guneshwarifegade@gmail.com>
2022-06-14 06:28:37 +0530

    [ovi]create database

---
+++ ADO_AddressBook/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ADO_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
